8924021504e2946cf8353ed721b02f2d9c7304eb
worksmile <mahaixiawork@163.com>
2018-05-29 17:13:04 +0800

    complite

--- src/js/index.js
+++ src/js/index.js
@@ -1,65 +1,90 @@
-var $ = window.Zepto; 
+var $ = window.Zepto;
 var root = window.player;
-$scope = $(document.body);
-// console.log(window);
-//绑定上一首下一首
-var index = 0;
-var songList ;
-
-
-function getDate(url){
+var $scope = $(document.body);
+var songList;
+var controlmanager;
+var audio = new root.audioManager();
+function bindClick(){
+    $scope.on("play:change",function(event,index,flag){
+        audio.setAudioSource(songList[index].audio);
+        if(audio.status == "play"||flag){
+            audio.play();
+            root.processor.start();
+        }
+        root.processor.renderAllTime(songList[index].duration)
+        root.render(songList[index]);
+        root.processor.updata(0);
+    })
+    //移动端click有300ms延迟
+    $scope.on("click",".prev-btn",function(){
+        var index = controlmanager.prev();
+        $scope.trigger("play:change",index);
+    })
+    $scope.on("click",".next-btn",function(){
+        var index = controlmanager.next();
+        $scope.trigger("play:change",index);
+    })
+    $scope.on("click",".play-btn",function(){
+        if(audio.status == "play"){
+            audio.pause();
+            root.processor.stop();
+        }else{
+            root.processor.start();
+            audio.play();
+        }
+        $(this).toggleClass("playing");
+    })
+    $scope.on("click",".list-btn",function(){
+        root.playList.show(controlmanager);
+    })
+}
+function bindTouch(){
+    var $slidePoint = $scope.find(".slider-point");
+    var offset = $scope.find(".pro-wrapper").offset();
+    var left = offset.left;
+    var width = offset.width;
+    //绑定拖拽事件 开始拖拽 ： 取消进度条渲染
+    $slidePoint.on("touchstart",function(){
+        root.processor.stop();
+    }).on("touchmove",function(e){
+        //计算拖拽的百分比 更新我们的当前时间和进度条
+        var x = e.changedTouches[0].clientX;
+        var percent = (x - left) / width;
+        if(percent > 1 || percent < 0){
+            percent = 0;
+        }
+        root.processor.updata(percent)
+    }).on("touchend",function(e){
+        //计算百分百 跳转播放 重新开始进度条渲染 
+        var x = e.changedTouches[0].clientX;
+        var percent = (x - left) / width;
+        if(percent > 1 || percent < 0){
+            percent = 0;
+        }
+        var curDuration = songList[controlmanager.index].duration;
+        var curTime = curDuration * percent;
+        audio.jumpToplay(curTime);
+        root.processor.start(percent);
+        $scope.find(".play-btn").addClass("playing");
+    })
+}
+function getData(url){
     $.ajax({
-        type:'GET',
-        url:url,
-        success:function(data){
-            //console.log(data)
+        type : "GET",
+        url : url,
+        success : function(data){
+            bindClick();
+            bindTouch();
+            root.playList.renderList(data);
+            controlmanager = new root.controlManager(data.length);
             songList = data;
-            root.render(songList[index]);
-            bindEvent();
+            $scope.trigger("play:change",0);
+            
         },
-        error:function(){
-            console.log("error");
+        error : function(){
+            console.log("error")
         }
     })
 }
 
-getDate('../mock/data.json');
-
-function bindEvent(){
-    //上一首
-    $scope.on('click','.prev-btn',function(){
-        if(index==0){
-            index = songList.length-1
-        }else{
-            index -- ;
-        }
-        root.render(songList[index]);
-        
-    })
-
-    //下一首
-    $scope.on('click','.next-btn',function(){
-        if(index == songList.length-1){
-            index = 0;
-        }else{
-            index ++;
-        }
-        root.render(songList[index]);
-        
-    })
-
-    //播放
-    $scope.on('click','.play-btn',function(){
-        
-        
-        
-    })
-
-    //暂停
-    //$scope.on('click','.')
-
-    //播放列表
-    $scope.on('click','.list-btn',function(){
-        
-    })
-}
+getData("../mock/data.json")
--- src/js/render.js
+++ src/js/render.js
@@ -1,40 +1,33 @@
-//实现页面渲染
-//为防止全局污染
 (function($,root){
-    var $scope = $(document.body)
+    var $scope = $(document.body);
+    console.log(root)
+    //渲染当前这首歌的信息
     function renderInfo(info){
-        //console.log(info);
-        var html = `<div class="song-name">${info.song}</div>
-        <div class="singer-name">${info.singer}</div>
-        <div class="album-name">${info.album}</div>`;
-        //$(".song-info").append(html);
-        $scope.find('.song-info').html(html);
+        var html = '<div class="song-name">'+info.song+'</div>'+
+        '<div class="singer-name">'+info.singer+'</div>'+
+        '<div class="album-name">'+info.album+'</div>';
+        $scope.find(".song-info").html(html)
     }
-
-    function renderImage(src){
-        //console.log(src)
+    //渲染当前这首歌的图片
+    function renderImg(src){
         var img = new Image();
-        img.src = src;
         img.onload = function(){
             root.blurImg(img,$scope);
-            $scope.find('.song-img img').attr("src",src);
-            
+            $scope.find(".song-img img").attr("src",src)
         }
-        
+        img.src = src;
     }
-
     function renderIsLike(isLike){
         if(isLike){
             $scope.find(".like-btn").addClass("liking");
         }else{
             $scope.find(".like-btn").removeClass("liking");
+            
         }
     }
-
-    root.render = function render(info){
-        renderInfo(info);
-        renderImage(info.image);
-        renderIsLike(info.isLike);
+    root.render = function(data){
+        renderInfo(data);
+        renderImg(data.image);
+        renderIsLike(data.isLike)
     }
-
-}(window.Zepto,window.player||(window.player = {})))//通过window.player暴露函数
+})(window.Zepto,window.player || (window.player = {}))
